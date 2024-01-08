5d4b9866c0171fb4d60ebb61cbeddcc7de446b81
sidhartha2002 <mohantysidhartha2002@gmail.com>
2024-01-08 06:22:07 +0530

    done

--- app/(amieSO)/components/hero.tsx
+++ app/(amieSO)/components/hero.tsx
@@ -1,11 +1,8 @@
 export const Hero = () => {
   return (
     <section className="flex flex-col justify-center py-20">
-      <h1 className="mb-12 font-heading text-7xl">
-        PARTS
-        <br />
-        <span className="text-gray-300">AeroMake Aviation</span>
-      </h1>
+      <div className="mb-12 font-heading text-7xl font-extrabold">PARTS</div>
+      <div className="text-gray-400 text-5xl">AeroMake Aviation</div>
 
       <div className="aspect-video w-2/5 rounded-xl bg-black shadow-md">
         Fake video ;)
